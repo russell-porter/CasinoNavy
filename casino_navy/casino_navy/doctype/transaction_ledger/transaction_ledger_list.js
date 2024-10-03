7ef3334bf0aa76166286271f00d9b28b01dc69e2
Lewin Villar <lewin.villar@gmail.com>
2024-10-03 12:33:33 -0400

    fix bug on Transaction Ledger List

--- casino_navy/casino_navy/doctype/transaction_ledger/transaction_ledger_list.js
+++ casino_navy/casino_navy/doctype/transaction_ledger/transaction_ledger_list.js
@@ -5,7 +5,7 @@ frappe.listview_settings["Transaction Ledger"] = {
 	get_indicator: function (doc) {
 		const status_colors = {
 			Deposit: "green",
-			Withdrawal: "red",
+			Withdraw: "red",
 		};
 		return [__(doc.transaction_type), status_colors[doc.transaction_type], "transaction_type,=," + doc.transaction_type];
 	},
